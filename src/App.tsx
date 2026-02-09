4beef97314b9543602b13f3e90b89fafdec2d61c
vilnout <vilnout0@gmail.com>
2026-02-09 09:15:44 +0530

    Feat: Add base layout for main dashboard content

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,34 @@
 import MainLayout from "./components/layout/MainLayout";
 
 function App() {
+  const quickBoxesCss = "border-2 p-7 rounded-lg";
   return (
     <>
-      <MainLayout></MainLayout>
+      <MainLayout>
+        <div className="space-y-6">
+          <h2 className="text-red-500">Dashboard</h2>
+          <p>Overview of your Financial Health</p>
+        </div>
+        <div>
+          <div className="my-4 grid grid-cols-2 gap-2 md:grid-cols-4">
+            <div className={quickBoxesCss}>Total Balance</div>
+            <div className={quickBoxesCss}>Income (Nov)</div>
+            <div className={quickBoxesCss}>Expenses (Nov)</div>
+            <div className={quickBoxesCss}>Savings Rate</div>
+          </div>
+          <div className="my-4 min-h-50 gap-3.5 rounded-lg border-2">
+            Main Chart Area
+          </div>
+          <div className="grid-col1 grid gap-2 md:grid-cols-2">
+            <div className="min-h-20 rounded-lg border-2">
+              <span>Recent Activity</span>
+            </div>
+            <div className="min-h-20 rounded-lg border-2">
+              <span>Spending by Category</span>
+            </div>
+          </div>
+        </div>
+      </MainLayout>
     </>
   );
 }
--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,10 +1,28 @@
+import type { ReactNode } from "react";
 import SideBar from "./Sidebar";
+import { Menu } from "lucide-react";
 
-function MainLayout() {
+interface MainLayoutProps {
+  children: ReactNode;
+}
+
+function MainLayout({ children }: MainLayoutProps) {
   return (
-    <>
-      <SideBar></SideBar>
-    </>
+    <div className="min-h-screen">
+      <SideBar />
+
+      {/* Mobile header */}
+      <div className="flex items-center justify-between border-b border-slate-700 p-4 md:hidden">
+        <div className="font-bold text-blue-500">Finance</div>
+        <button className="font-bold">
+          <Menu size={24} />
+        </button>
+      </div>
+
+      <main className="p-4 md:ml-64 md:p-8">
+        <div className="mx-auto max-w-7xl">{children}</div>
+      </main>
+    </div>
   );
 }
 
