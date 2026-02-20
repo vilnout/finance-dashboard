e54d069bfa640543728eb59dd39cf88bf546ee42
vilnout <vilnout0@gmail.com>
2026-02-20 17:21:04 +0530

    refactor: update component to accept arbitary tailwind classes

--- src/components/ui/AddTransactionButton.tsx
+++ src/components/ui/AddTransactionButton.tsx
@@ -1,14 +1,16 @@
 type AddTransactionButtonProps = {
   onClick: () => void;
+  className?: string;
 };
 
 export const AddTransactionButton = ({
   onClick,
+  className = "",
 }: AddTransactionButtonProps) => {
   return (
     <button
       onClick={onClick}
-      className="cursor-pointer rounded-lg bg-blue-600 p-3 font-bold text-white transition-colors hover:bg-blue-500"
+      className={`${className} cursor-pointer rounded-lg bg-blue-600 p-3 font-bold text-white transition-colors hover:bg-blue-500`}
     >
       Add Transaction
     </button>
