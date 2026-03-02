841d4ccb98f72a7aa516aed814e5913caa4d793d
vilnout <vilnout0@gmail.com>
2026-03-02 10:59:55 +0530

    chore: update mock data generation

--- src/data/mockData.ts
+++ src/data/mockData.ts
@@ -11,11 +11,13 @@ export const MOCK_BUDGETS: Budget[] = [
 const generateTransactions = (): Transaction[] => {
   const transactions: Transaction[] = [];
   const today = new Date();
+  const day = today.getDay();
 
-  for (let i = 0; i < 8; i++) {
+  for (let i = 0; i < 9; i++) {
     const targetMonth = new Date(today.getFullYear(), today.getMonth() - i, 1);
     const month = targetMonth.getMonth();
     const year = targetMonth.getFullYear();
+    const currentMonth = i === 0;
 
     // Simulate fixed expenses
     transactions.push({
@@ -28,38 +30,62 @@ const generateTransactions = (): Transaction[] => {
     transactions.push({
       id: crypto.randomUUID(),
       date: new Date(year, month, 1),
+      amount: Math.random() * 2000,
+      description: "Side Jobs",
+      category: "Income",
+    });
+    transactions.push({
+      id: crypto.randomUUID(),
+      date: new Date(year, month, 1),
       amount: -1700,
       description: "Apartment Rent",
       category: "Housing",
     });
-    transactions.push({
-      id: crypto.randomUUID(),
-      date: new Date(year, month, 15),
-      amount: -(150 + Math.floor(Math.random() * 50)),
-      description: "Electricity & Water",
-      category: "Utilities",
-    });
-    // Push to over budget
-    transactions.push({
-      id: crypto.randomUUID(),
-      date: new Date(year, month, 15),
-      amount: -(300 + Math.random() * 20),
-      description: "Date",
-      category: "Entertainment",
-    });
+
+    if (!(i === 0 && day < 15)) {
+      transactions.push({
+        id: crypto.randomUUID(),
+        date: new Date(year, month, 15),
+        amount: -(150 + Math.floor(Math.random() * 50)),
+        description: "Electricity & Water",
+        category: "Utilities",
+      });
+      // Push to over budget
+      transactions.push({
+        id: crypto.randomUUID(),
+        date: new Date(year, month, 19),
+        amount: -(300 + Math.random() * 20),
+        description: "Date",
+        category: "Entertainment",
+      });
+    }
 
     // Simulate variable expenses
     for (let j = 0; j < 12; j++) {
-      const randomDay = Math.floor(Math.random() * 28) + 1;
+      const randomDay =
+        Math.floor(Math.random() * (currentMonth ? day : 28)) + 1;
       const isWeekend = new Date(year, month, randomDay).getDay() % 6 === 0;
 
-      transactions.push({
-        id: crypto.randomUUID(),
-        date: new Date(year, month, randomDay),
-        amount: -(10 + Math.random() * 10),
-        description: "Subway",
-        category: "Transport",
-      });
+      if (Math.random() < 0.4) {
+        transactions.push({
+          id: crypto.randomUUID(),
+          date: new Date(year, month, randomDay),
+          amount: -(10 + Math.random() * 10),
+          description: "Subway",
+          category: "Transport",
+        });
+      }
+
+      // Chance of a massive unexpected expense
+      if (j == 0 && Math.random() < 0.4) {
+        transactions.push({
+          id: crypto.randomUUID(),
+          date: new Date(year, month, randomDay),
+          amount: -(1000 + Math.random() * 2000),
+          description: "Medical Bills",
+          category: "Other",
+        });
+      }
 
       if (j % 3 === 0) {
         transactions.push({
@@ -74,7 +100,7 @@ const generateTransactions = (): Transaction[] => {
       if (isWeekend && j % 2 === 0) {
         transactions.push({
           id: crypto.randomUUID(),
-          date: new Date(year, month, 15),
+          date: new Date(year, month, randomDay),
           amount: -(50 + Math.random() * 50),
           description: "Restaurant / Movie",
           category: "Entertainment",
