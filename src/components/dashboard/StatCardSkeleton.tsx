c3ee8e3dc71a340e474746a8556c09b43b21b953
vilnout <vilnout0@gmail.com>
2026-03-03 09:09:31 +0530

    style: add margin to stat card skeleton

--- src/components/dashboard/StatCardSkeleton.tsx
+++ src/components/dashboard/StatCardSkeleton.tsx
@@ -13,7 +13,7 @@ export const StatCardSkeleton = () => {
     <div className="flex h-[104px] items-center justify-between rounded-xl border border-slate-800 bg-slate-900 p-6">
       <div>
         <Skeleton className="h-4 w-24" />
-        <Skeleton className="h-8 w-32" />
+        <Skeleton className="mt-2 h-8 w-32" />
       </div>
       <Skeleton className="h-12 w-12 rounded-lg" />
     </div>
